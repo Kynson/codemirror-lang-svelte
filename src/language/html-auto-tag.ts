52a907609919e5009ea9b2ad5a017c982f2f1933
Kynson Szetau <46522440+Kynson@users.noreply.github.com>
2026-01-01 09:03:51 +0800

    Bug fix for auto close tag

--- src/language/html-auto-tag.ts
+++ src/language/html-auto-tag.ts
@@ -29,98 +29,77 @@ function getElementName(
     : '';
 }
 
-export const autoCloseTags = EditorView.inputHandler.of(
-  (view, from, to, text) => {
-    const currentNode = syntaxTree(view.state).resolveInner(from, -1);
-    const potentialElementNode =
-      currentNode.name === 'SvelteElementType'
-        ? // SvelteElementType -> SvelteElementName -> OpenTag -> Element
-          currentNode.parent?.parent?.parent
-        : // TagName (if correct position) -> OpenTag -> Element
-          currentNode.parent?.parent;
+export const autoCloseTags = EditorView.inputHandler.of((view, from, to, text) => {
+  if (
+    view.composing ||
+    view.state.readOnly ||
+    from !== to ||
+    (text !== '>' && text !== '/')
+  ) {
+    return false;
+  }
+
+  const { state } = view;
+  const changes = state.changeByRange((range) => {
+    const { head } = range;
+    let around: SyntaxNode | null = syntaxTree(state).resolveInner(head, -1);
+    let name: string;
+
+    // Tree structure: Element -> OpenTag -> ...
+    while (around && around.name !== 'OpenTag') {
+      around = around.parent;
+    }
+
+    if (!around) {
+      return { range };
+    }
+
+    name = getElementName(state.doc, around.parent, head);
+    const nextChar = state.doc.sliceString(head, head + 1);
 
     if (
-      view.composing ||
-      view.state.readOnly ||
-      from !== to ||
-      (text !== '>' && text !== '/') ||
-      potentialElementNode?.name !== 'Element'
+      text === '>' &&
+      // Ensure there is no closing tag already
+      (around.parent?.lastChild?.name !== 'CloseTag' || nextChar !== '<') &&
+      name
     ) {
-      return false;
+      const hasRightBracket = nextChar === '>';
+      const insert = `${hasRightBracket ? '' : '>'}</${name}>`;
+      return {
+        range: EditorSelection.cursor(head + 1),
+        changes: { from: head + (hasRightBracket ? 1 : 0), insert },
+      };
     }
 
-    const { state } = view;
-    const changes = state.changeByRange((range) => {
-      const { head } = range;
-      let around = syntaxTree(state).resolveInner(head, -1);
-      let name: string;
+    const empty = around.parent;
+    const base = empty?.parent;
+    name = getElementName(state.doc, base, head);
 
-      // The below node must have a parent node as they are inside a Document/ OpenTag
-      if (
-        around.name === 'TagName' ||
-        around.name === 'ComponentName' ||
-        around.name === 'StartTag'
-      ) {
-        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-        around = around.parent!;
-      }
+    if (
+      text === '/' &&
+      empty?.from === head - 1 &&
+      base?.lastChild?.name !== 'CloseTag' &&
+      name
+    ) {
+      const hasRightBracket =
+        view.state.doc.sliceString(head, head + 1) === '>';
+      const insert = `/${name}${hasRightBracket ? '' : '>'}`;
+      const pos = head + insert.length + (hasRightBracket ? 1 : 0);
 
-      if (around.name === 'SvelteElementType') {
-        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-        around = around.parent!.parent!;
-      }
-
-      name = getElementName(state.doc, around.parent, head);
-      const nextChar = state.doc.sliceString(head, head + 1);
-
-      console.log('Element name:', name);
-
-      if (
-        text === '>' &&
-        around.name === 'OpenTag' &&
-        // Ensure there is no closing tag already
-        (around.parent?.lastChild?.name !== 'CloseTag' || nextChar !== '<') &&
-        name
-      ) {
-        const hasRightBracket = nextChar === '>';
-        const insert = `${hasRightBracket ? '' : '>'}</${name}>`;
-        return {
-          range: EditorSelection.cursor(head + 1),
-          changes: { from: head + (hasRightBracket ? 1 : 0), insert },
-        };
-      }
-
-      const empty = around.parent;
-      const base = empty?.parent;
-      name = getElementName(state.doc, base, head);
-
-      if (
-        text === '/' &&
-        around.name === 'OpenTag' &&
-        empty?.from === head - 1 &&
-        base?.lastChild?.name !== 'CloseTag' &&
-        name
-      ) {
-        const hasRightBracket =
-          view.state.doc.sliceString(head, head + 1) === '>';
-        const insert = `/${name}${hasRightBracket ? '' : '>'}`;
-        const pos = head + insert.length + (hasRightBracket ? 1 : 0);
-
-        return {
-          range: EditorSelection.cursor(pos),
-          changes: { from: head, insert },
-        };
-      }
-
-      return { range };
-    });
-
-    if (changes.changes.empty) {
-      return false;
+      return {
+        range: EditorSelection.cursor(pos),
+        changes: { from: head, insert },
+      };
     }
 
-    view.dispatch(changes, { userEvent: 'input.type', scrollIntoView: true });
+    return { range };
+  });
 
-    return true;
+  if (changes.changes.empty) {
+    return false;
   }
-);
+
+  view.dispatch(changes, { userEvent: 'input.type', scrollIntoView: true });
+
+  return true;
+});
